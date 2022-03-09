977eeac0acf019f18f2a7ac8c5ceb0e67ad7b1fb
m.andronov <m.andronov@m-games-ltd.com>
2022-03-09 22:54:54 +0300

    add db connection

---
+++ diplom/diplom/Data/diplomContext.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using diplom.Models;
+
+namespace diplom.Data
+{
+    public class diplomContext : DbContext
+    {
+
+        public diplomContext (DbContextOptions<diplomContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<diplom.Models.Company> Company { get; set; }
+    }
+}
---
+++ diplom/diplom/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace diplom.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? Symbol { get; set; }
+        public string? longName { get; set; }
+    }
+}
